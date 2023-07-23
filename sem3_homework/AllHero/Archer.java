98b1fb8f3075345967e1782935591404a02cf4e5
konopleva-nina <122988231+konopleva-nina@users.noreply.github.com>
2023-07-23 21:12:47 +0300

    Update Archer.java

--- sem3_homework/AllHero/Archer.java
+++ sem3_homework/AllHero/Archer.java
@@ -1,12 +1,1 @@
-package AllHero;
 
-public class Archer extends Shooter{
-    public Archer(int x, int y, int initiative) {
-        super(x, y, initiative + 3, 5, 20, 0);
-    }
-
-    @Override
-    public String getInfo() {
-        return "Archer [" + coordinates.x + ", " + coordinates.y + "] arrows: " + arrows + " HP: " + hp + "/" + max_hp + " " + state;
-    }
-}
