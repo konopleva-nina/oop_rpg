4a93769c6af15db9c4062b98954b6b12d17446b5
konopleva-nina <122988231+konopleva-nina@users.noreply.github.com>
2023-07-23 21:17:09 +0300

    Update Sniper.java

--- sem3_homework/AllHero/Sniper.java
+++ sem3_homework/AllHero/Sniper.java
@@ -1,14 +1,1 @@
-package AllHero;
 
-import java.util.ArrayList;
-
-public class Sniper extends Shooter {
-
-    public Sniper(int x, int y, int initiative) {
-        super(x, y, initiative + 3, 7, 100, 1);
-    }
-    @Override
-    public String getInfo() {
-        return "Sniper [" + coordinates.x + ", " + coordinates.y + "] HP: " + hp + "/" + max_hp + " " + state;
-    }
-}
