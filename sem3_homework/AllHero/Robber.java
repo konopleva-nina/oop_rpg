c9374b506af0d2d534aba165a1799ecba142356e
konopleva-nina <122988231+konopleva-nina@users.noreply.github.com>
2023-07-23 21:16:34 +0300

    Update Robber.java

--- sem3_homework/AllHero/Robber.java
+++ sem3_homework/AllHero/Robber.java
@@ -1,14 +1,1 @@
-package AllHero;
 
-import java.util.ArrayList;
-
-public class Robber extends Melee{
-    public Robber(int x, int y, int initiative) {
-        super (x, y, initiative + 4, 5, 2, 2, 1);
-    }
-
-    @Override
-    public String getInfo() {
-        return "Robber [" + coordinates.x + ", " + coordinates.y + "] HP: " + hp + "/" + max_hp + " " + state;
-    }
-}
